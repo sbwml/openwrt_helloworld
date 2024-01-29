Jackie264/openwrt-passwall2

luci: sing-box 1.8.0 Migrate cache file options
* Migrate cache file from Clash API to independent options

* Not compatible with the sing-box old version, please update to version 1.8.0 or higher.

luci-app-passwall2/luasrc/passwall2/util_sing-box.lua
@@ -1304,15 +1304,14 @@ function gen_config(var)
 				strategy = remote_strategy,
 			})
 
-			if tags and tags:find("with_clash_api") then
-				if not experimental then
-					experimental = {}
-				end
-				experimental.clash_api = {
-					store_fakeip = true,
-					cache_file = "/tmp/singbox_passwall2_" .. flag .. ".db"
-				}
+			if not experimental then
+				experimental = {}
 			end
+			experimental.cache_file = {
+				enabled = true,
+				store_fakeip = true,
+				path = "/tmp/singbox_passwall2_" .. flag .. ".db"
+			}
 		end
 	
 		if direct_dns_udp_server then

sbwml/openwrt_helloworld

luci-app-passwall/luasrc/passwall/util_sing-box.lua
@@ -1310,15 +1310,14 @@ function gen_config(var)
 				strategy = remote_strategy,
 			})
 
-			if tags and tags:find("with_clash_api") then
-				if not experimental then
-					experimental = {}
-				end
-				experimental.clash_api = {
-					store_fakeip = true,
-					cache_file = "/tmp/singbox_passwall_" .. flag .. ".db"
-				}
+			if not experimental then
+				experimental = {}
 			end
+			experimental.cache_file = {
+				enabled = true,
+				store_fakeip = true,
+				path = "/tmp/singbox_passwall_" .. flag .. ".db"
+			}
 		end
 	
 		if direct_dns_udp_server then