c9976e749a9421eca65a2fdfd317ed00e3c0d434
actions <action@github.com>
2024-04-08 08:37:30 +0800

    luci-app-passwall: sync upstream

--- luci-app-passwall/root/usr/share/passwall/app.sh
+++ luci-app-passwall/root/usr/share/passwall/app.sh
@@ -1576,16 +1576,6 @@ acl_app() {
 	}
 }
 
-boot() {
-	local delay=$(config_t_get global_delay start_delay 1)
-	if [ "$delay" -gt 0 ]; then
-		echolog "执行启动延时 $delay 秒后再启动!"
-		sleep $delay && start >/dev/null 2>&1 &
-	else
-		start
-	fi
-}
-
 start() {
 	ulimit -n 65535
 	start_haproxy
@@ -1752,8 +1742,5 @@ stop)
 	;;
 start)
 	start
-	;;
-boot)
-	boot
 	;;
 esac
